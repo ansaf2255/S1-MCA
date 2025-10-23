40a54b5022c08daca48c680986629a8c6f8f5135
ansaf2255 <ansafpbashir@gmail.com>
2025-10-23 15:41:46 +0530

    bst

--- binarytree.c
+++ binarytree.c
@@ -8,7 +8,7 @@ struct node
     struct node *left;
 };
 
-struct node* head=NULL;
+struct node* root=NULL;
 
 struct node * createnode(int n)
 {
@@ -17,6 +17,67 @@ struct node * createnode(int n)
     newnode->right=NULL;
     newnode->left=NULL;
     return newnode;
+}
+
+void insert(int n)
+{
+    struct node *newnode=createnode(n);
+    struct node *ptr=root;
+    struct node *ptr1=root;
+    if(root==NULL)
+    {
+        root=newnode;
+    }
+    else
+    {
+        while(ptr!=NULL)
+        {  
+            if (newnode->data < ptr->data)
+            {
+                ptr1=ptr;
+                ptr=ptr->left;
+            }
+            else
+            {
+                ptr1=ptr;
+                ptr=ptr->right;
+            }
+        }
+        if(ptr==NULL)
+        {
+            if(ptr1->data > newnode->data)
+            {
+                ptr1->left=newnode;
+            }
+            else
+            {
+                ptr1->right=newnode;
+            }
+        }
+           
+    }
+}
+
+void preorder(struct node *root)
+{
+     if (root != NULL) 
+    {
+        printf("%d ", root->data);  
+        preorder(root->left);       
+        preorder(root->right);      
+    }
+}
+
+int main()
+{
+    insert(50);
+    insert(30);
+    insert(70);
+    insert(20);
+    insert(40);
+    insert(60);
+    insert(80);
+    preorder(root);
 }
 
 
