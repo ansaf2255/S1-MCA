7c08883f81223e8239315c06ad8d8ea384ed50f9
ansaf2255 <ansafpbashir@gmail.com>
2025-10-23 13:22:46 +0530

    py24

--- lab21.py
+++ lab21.py
@@ -3,7 +3,7 @@ a=0
 b=1
 print("fibonacci series:")
 print(a)
-for i in range(0,n+1):
+for i in range(0,n-1):
     s=a+b
     a=b
     b=s
---
+++ lab22.py
@@ -0,0 +1,8 @@
+a=input("enter a string:")
+freq={}
+for c in a:
+    if c in freq:
+        freq[c]+=1
+    else:
+        freq[c]=1
+print(freq)
