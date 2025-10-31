45c5d04caa9ce54ef45ffb03e2e88e7862fbb2cc
ansaf2255 <ansafpbashir@gmail.com>
2025-10-31 12:02:36 +0530

    binary tree deletion

--- binarytree.c
+++ binarytree.c
@@ -58,9 +58,95 @@ void insert(int n)
     }
 }
 
+struct node* succ(struct node *ptr)
+{
+    struct node *ptr1=ptr->right;
+    
+    while (ptr1->left!=NULL)
+    {
+        ptr1=ptr1->left;
+    }
+    return ptr1;
+}
+
 void delete(int n)
 {
+    int flag=0,item;
+    struct node *ptr=root;
+    struct node *ptr1=root;
+    struct node *ptr2;
 
+    //struct node *ptr2;
+    printf("Enter the value to be deleted");
+    scanf("%d",&n);
+    if(root==NULL)
+    {
+        printf("Tree Empty");
+    }
+    else
+    {
+            while(ptr!=NULL)
+            {
+                if(n < ptr->data)
+                {
+                    ptr1=ptr;
+                    ptr=ptr->left;
+                }
+                else if(n > ptr->data)
+                {
+                    ptr1=ptr;
+                    ptr=ptr->right;
+                }
+                else
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                printf("element doesnt exist");
+            }
+        else if(ptr->left==NULL && ptr->right==NULL)
+        {
+            if(ptr1->left==ptr)
+            {
+                ptr1->left=NULL;
+            }
+            else
+            {
+                ptr1->right=NULL;
+            }
+        }
+        else if(ptr->left!=NULL && ptr->right==NULL && ptr1->left==ptr)
+        {
+            ptr1->left=ptr->left;
+            free(ptr);
+        }
+        else if(ptr->left!=NULL && ptr->right==NULL && ptr1->right==ptr)
+        {
+            ptr1->right=ptr->left;
+            free(ptr);
+        }
+        else if(ptr->left==NULL && ptr->right!=NULL && ptr1->left==ptr)
+        {
+            ptr1->left=ptr->right;
+            free(ptr);
+        }
+        else if(ptr->left==NULL && ptr->right!=NULL && ptr1->right==ptr)
+        {
+            ptr1->right=ptr->right;
+            free(ptr);
+        }
+        else
+        {
+            ptr2=succ(ptr);
+            item=ptr2->data;
+            ptr->data=item;
+            free(ptr2);
+//not working
+        }
+    }
 } 
 
 void search(int n ,struct node * root)
@@ -72,10 +158,7 @@ void search(int n ,struct node * root)
     else if(n>root->data,root->right)
     {
         search(n,root->right);
-
     }   
-
-
 }
 
 void preorder(struct node *root)
@@ -86,16 +169,15 @@ void preorder(struct node *root)
         preorder(root->left);       
         preorder(root->right);      
     }
-
+}
 
 void postorder(struct node *root)
 {
      if (root != NULL) 
     {
-      
        postorder(root->left);       
        postorder(root->right); 
-        printf("%d ", root->data);       
+       printf("%d ", root->data);       
     }
 }
 
@@ -105,14 +187,13 @@ void inorder(struct node *root)
     {
       
        inorder(root->left);
-         printf("%d ", root->data);          
+       printf("%d ", root->data);          
        inorder(root->right); 
-          
     }
 }
 
 int main()
-{
+{int n;
     insert(50);
     insert(30);
     insert(70);
@@ -121,6 +202,13 @@ int main()
     insert(60);
     insert(80);
     preorder(root);
+    delete(n);
+    preorder(root);
+    delete(n);
+    preorder(root);
+    delete(n);
+    preorder(root);
+
 }
 
 
